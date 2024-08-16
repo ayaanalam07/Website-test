8af4b7cb6713dbfb175b8384eedfa39dba79b602
ayaanalam07 <ayaanabaf@gmail.com>
2024-08-16 09:42:08 +0500

    pending

--- app.js
+++ app.js
@@ -3,25 +3,42 @@ import { auth } from "./firebaseconfig.js";
 
 
 const logout = document.querySelector('#Logout')
+const btnDiv = document.querySelector("#btnDiv");
 
 
 
 
-
-
+const onAuth = 
 onAuthStateChanged(auth, (user) => {
-    if (user) {
-      const uid = user.uid;
-      console.log(uid);
-     
-    } else {
-     window.location="login.html"
-    }
+  if (user) {
+    const uid = user.uid;
+    console.log(uid);
+   
+  } else {
+   
+   btnDiv.innerHTML = `
+  <a class="link link-primary" href="./login.html">Login</a>
+
+   `
+  }
+});
+logout.addEventListener('click',()=>{
+  signOut(auth).then(() => {
+  window.location = 'login.html'
+  }).catch((error) => {
+    console.log(error);
   });
-  logout.addEventListener('click',()=>{
-    signOut(auth).then(() => {
-    window.location = 'login.html'
-    }).catch((error) => {
-      // An error happened.
-    });
-  })
+})
+
+import { collection, addDoc } from "firebase/firestore"; 
+
+try {
+  const docRef = await addDoc(collection(db, "users"), {
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
